ef79e4cd83468f16f31cb259094e58a618068095
torrinconrath <torrin@vt.edu>
2024-01-02 17:26:29 -0500

    changes

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
-import subprocess
+# app.py
+
+from flask import Flask, render_template
+from agentvsghosts import start_game
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/launch_game')
 def launch_game():
-    subprocess.run(['python', 'path/to/agentvsghosts.py'])
+    start_game()
     return 'Game launched!'
 
 if __name__ == '__main__':
