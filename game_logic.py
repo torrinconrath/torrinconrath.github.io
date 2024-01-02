616ed28fc20a8789cb968603f392fdeb9896bb92
torrinconrath <torrin@vt.edu>
2024-01-02 17:00:47 -0500

    change

---
+++ game_logic.py
@@ -0,0 +1,7 @@
+# game_logic.py
+
+def run_game():
+    import agentvsghosts  # Import the existing code from agentvsghosts.py
+
+    # Call the main function in agentvsghosts.py
+    agentvsghosts.main()
