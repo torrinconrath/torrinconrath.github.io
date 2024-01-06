425231e54f6ebcc5c56e8b210acdef78b3b9f107
torrinconrath <torrin@vt.edu>
2024-01-06 17:54:45 -0500

    all in one file

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
 # run.py
 
-from app import app
+from flask import Flask, render_template
+from agentvsghosts import run_game  # Assuming this is where your Pygame logic is
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('game_page.html')
+
+@app.route('/launch_game')
+def launch_game():
+    run_game()  # Call the function to execute game logic
+    return 'Game launched!'
 
 if __name__ == '__main__':
     app.run(debug=True)
